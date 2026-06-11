2ed9307c63d17a50ef807aac601e7e3f9746bfb4
Shaik <shaik@brokerseye.com>
2026-06-11 10:41:45 +0400

    Seaprate DB

--- Back-End/config/settings.py
+++ Back-End/config/settings.py
@@ -109,9 +109,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dwms',
         'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '10.255.254.25',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
